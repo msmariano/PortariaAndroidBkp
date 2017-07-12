019af31e9cb566f251911420d94a0f1eca20fc4c
msmariano <#Z1lm@200677>
2017-07-12 12:32:10 -0300

    .

--- app/src/main/java/com/projetos/marcelo/portaria/MediaPlayerService.java
+++ app/src/main/java/com/projetos/marcelo/portaria/MediaPlayerService.java
@@ -1,5 +1,6 @@
 package com.projetos.marcelo.portaria;
 
+import android.*;
 import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -9,6 +10,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.media.session.MediaController;
@@ -17,8 +21,15 @@ import android.media.session.MediaSessionManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -28,24 +39,32 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.List;
+import java.util.Locale;
 
-public class MediaPlayerService extends Service {
+public class MediaPlayerService extends Service implements LocationListener {
 
     public static final String ACTION_PLAY = "action_play";
     public static final String ACTION_PAUSE = "action_pause";
     public static final String ACTION_STOP = "action_stop";
-
-
+    private LocationManager locationManager;
     private MediaPlayer mMediaPlayer;
     private MediaSessionManager mManager;
     private MediaSession mSession;
     private MediaController mController;
     private boolean connected = false;
     private boolean connectedOnline = false;
-    String msg ="";
+    NotificationManager manager;
+    private Notification.Builder builder;
+    private NotificationManager notificationManager;
+    LocationListener locationListenerGps;
+    String msg = "";
     Socket socket;
     PrintWriter out;
-
+    Thread cThreadOnline;
+    public double dLatitude, dLongitude;
+    public Location org;
+    MediaPlayerService localObj;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -53,151 +72,189 @@ public class MediaPlayerService extends Service {
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void handleIntent( Intent intent ) {
-        if( intent == null || intent.getAction() == null )
+    private void handleIntent(Intent intent) {
+        if (intent == null || intent.getAction() == null)
             return;
 
         String action = intent.getAction();
 
-        if( action.equalsIgnoreCase( ACTION_PLAY ) ) {
+        if (action.equalsIgnoreCase(ACTION_PLAY)) {
             mController.getTransportControls().play();
-        }
-        else if( action.equalsIgnoreCase( ACTION_PAUSE ) ) {
+        } else if (action.equalsIgnoreCase(ACTION_PAUSE)) {
             mController.getTransportControls().pause();
-        }
-        else if( action.equalsIgnoreCase( ACTION_STOP ) ) {
+        } else if (action.equalsIgnoreCase(ACTION_STOP)) {
             mController.getTransportControls().stop();
         }
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
 
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            String rua = "";
+            String numero = "";
+            if(addresses.get(0).getThoroughfare() != null)
+                rua = addresses.get(0).getThoroughfare();
+            if(addresses.get(0).getSubThoroughfare() != null)
+                numero = addresses.get(0).getSubThoroughfare();
+
+            if(rua.trim().toLowerCase().equals("rua cyro vellozo") && numero.trim().toLowerCase().equals("56")){
+                updateContext("Em casa["+rua+" "+numero+"]");
+
+            }
+            else
+                updateContext("Fora de Casa["+rua+" "+numero+"]");
+
+            float[] dist = new float[1];
+            dist[0] = 0;
+            Location.distanceBetween(dLatitude, dLongitude, location.getLatitude(), location.getLongitude(), dist);
+
+        } catch (IOException e) {
+            //updateContext("Erro ao localizar : "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 
     @TargetApi(Build.VERSION_CODES.KITKAT_WATCH)
-    private Notification.Action generateAction( int icon, String title, String intentAction ) {
-        Intent intent = new Intent( getApplicationContext(), MediaPlayerService.class );
-        intent.setAction( intentAction );
+    private Notification.Action generateAction(int icon, String title, String intentAction) {
+        Intent intent = new Intent(getApplicationContext(), MediaPlayerService.class);
+        intent.setAction(intentAction);
         PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), 1, intent, 0);
-        return new Notification.Action.Builder( icon, title, pendingIntent ).build();
+        return new Notification.Action.Builder(icon, title, pendingIntent).build();
     }
-
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void buildNotification( Notification.Action action ) {
+    private void buildNotification(Notification.Action action) {
         Notification.MediaStyle style = new Notification.MediaStyle();
-
-        Intent intent = new Intent( getApplicationContext(), MediaPlayerService.class );
+        Intent intent = new Intent(getApplicationContext(), MediaPlayerService.class);
         intent.setAction(ACTION_STOP);
         PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), 1, intent, 0);
-        Notification.Builder builder = new Notification.Builder( this )
-                .setSmallIcon(R.drawable.ic_launcher2)
-                .setColor(0)
-                .setContentTitle( "ControleRemoto" )
-                .setContentText( "" )
-                .setDeleteIntent(pendingIntent)
-                .setStyle(style);
+        builder = new Notification.Builder(this).setSmallIcon(R.drawable.ic_launcher2).setColor(0)
+                .setContentTitle("Controle Remoto").setContentText("").setDeleteIntent(pendingIntent).setStyle(style);
         builder.setTicker("this is ticker text");
-
-
-        Thread cThreadOnline = new Thread(new ClientThreadOnline());
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                updateContext("GPS Permitido");
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                updateContext("Iniciando GPS!");
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                if (locationManager != null) {
+                    Location location = locationManager
+                            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        Geocoder geocoder;
+                        List<Address> addresses;
+                        geocoder = new Geocoder(this, Locale.getDefault());
+                        try {
+                            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                            String rua = "";
+                            String numero = "";
+                            if(addresses.get(0).getThoroughfare() != null)
+                                rua = addresses.get(0).getThoroughfare();
+                            if(addresses.get(0).getSubThoroughfare() != null)
+                                numero = addresses.get(0).getSubThoroughfare();
+                            updateContext(rua+" "+numero);
+                        } catch (IOException e) {
+                            updateContext("Erro ao localizar : "+e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        catch(Exception e){
+            updateContext(e.getMessage());
+        }
+        cThreadOnline = new Thread(new ClientThreadOnline());
         cThreadOnline.start();
 
-        /*builder.setVibrate(new long[]{100, 250, 100, 500});
-        builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));*/
+		/*
+		 * builder.setVibrate(new long[]{100, 250, 100, 500});
+		 * builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.
+		 * TYPE_NOTIFICATION));
+		 */
         builder.addAction(action);
         style.setShowActionsInCompactView(0);
-
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService( Context.NOTIFICATION_SERVICE );
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(1, builder.build());
     }
-    public class ClientThreadOnline implements Runnable {
-        private String line;
 
-        public void run()
+
+
+    public void updateContext(String mens){
+        try {
+            builder.setContentText(mens);
+            notificationManager.notify(1, builder.build());
+        }
+        catch (Exception e)
         {
-              while(true){
-                    try {
-                        Thread.sleep(10000);
-                    } catch (Exception e) {
-                    }
-                    //showNotification();
-
-
-                   Log.i("SERVICE", "Service running");
-
-                }
 
         }
     }
 
-    private void showNotification() {
+    public class ClientThreadOnline implements Runnable {
+        private String line;
 
+        public void run() {
+            boolean isRun = true;
+            int i = 0;
+            while (isRun) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    isRun = false;
+                }
+                i++;
 
-        Notification myNotication;
+                try {
+                       // updateContext("Contando : " + String.valueOf(i));
 
-        Bundle yesBundle = new Bundle();
-
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        Intent intent = new Intent("com.example.marcelo.controleremoto");
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, intent, 0);
-
-
-        Notification.Builder builder = new Notification.Builder(this);
-        //builder.addAction(0, "Atender", pendingIntentAtender);
-
-
-        builder.setAutoCancel(false);
-        builder.setTicker("this is ticker text");
-        builder.setContentTitle("Portaria");
-        builder.setContentText("Tocando campainha.Por favor atenda!");
-
-        yesBundle.putInt("userAnswer", 1);//This is the value I want to pass
-        intent.putExtras(yesBundle);
-        intent.setAction("call_method");
-        PendingIntent pendingIntentAtender = PendingIntent.getBroadcast(this, 12345, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.addAction(0, "Atender", pendingIntentAtender);
-
-        builder.setSmallIcon(R.drawable.chat1);
-        builder.setContentIntent(pendingIntent);
-        builder.setOngoing(true);
-        builder.setSubText("Atenção...");   //API level 16
-        builder.setNumber(100);
-        builder.setVibrate(new long[]{100, 250, 100, 500});
-        builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
-
-
-        builder.build();
-
-
-        myNotication = builder.getNotification();
-        manager.notify(11, myNotication);
+                } catch (Exception e) {
+                    isRun = false;
+                }
+            }
+        }
     }
 
-    public class ClientThread implements Runnable
-    {
+    public class ClientThread implements Runnable {
 
-        public void run()
-        {
-            try
-            {
+        public void run() {
+            try {
                 InetAddress serverAddr = InetAddress.getByName("192.168.0.14");
-                Socket socket = new Socket(serverAddr,81);
+                Socket socket = new Socket(serverAddr, 81);
                 connected = true;
                 boolean bEnviado = false;
-                //Context context = getApplicationContext();
                 int duration = Toast.LENGTH_SHORT;
-                if (connected)
-                {
+                if (connected) {
                     try {
                         PrintWriter out = new PrintWriter(
-                                new BufferedWriter(new OutputStreamWriter(
-                                        socket.getOutputStream())), true);
+                                new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
                         out.println("act");
 
-                        BufferedReader in = new BufferedReader(
-                                new InputStreamReader(socket.getInputStream()));
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
                         try {
                             StringBuilder total = new StringBuilder();
@@ -206,31 +263,21 @@ public class MediaPlayerService extends Service {
                                 total.append(line);
                             }
                             msg = total.toString().trim();
-                            //showToastInThread(MainActivity.this,msg);
-                            //Log.d("ClientActivity", msg);
-
-
+                            updateContext(msg);
                         } catch (IOException e) {
-                            //Toast toast = Toast.makeText(context, e.getMessage(), duration);
-                            //toast.show();
-                            //e.printStackTrace();
-                            Log.e("NETWORK-RECEIVE", "Something goes wrong: IOException",e);
+                            updateContext("Erro envio comando portão : "+e.getMessage());
                         }
 
-                    }
-                    catch (Exception e)
-                    {
-
+                    } catch (Exception e) {
+                        updateContext("Erro envio comando portão : "+e.getMessage());
                     }
 
                 }
                 socket.close();
-                Log.d("ClientActivity", "C: Closed.");
+
                 connected = false;
-            }
-            catch (Exception e)
-            {
-                Log.e("ClientActivity", "C: Error", e);
+            } catch (Exception e) {
+
                 connected = false;
             }
         }
@@ -238,7 +285,7 @@ public class MediaPlayerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if( mManager == null ) {
+        if (mManager == null) {
             initMediaSessions();
         }
         handleIntent(intent);
@@ -250,70 +297,29 @@ public class MediaPlayerService extends Service {
         mMediaPlayer = new MediaPlayer();
 
         mSession = new MediaSession(getApplicationContext(), "simple player session");
-        mController =new MediaController(getApplicationContext(), mSession.getSessionToken());
+        mController = new MediaController(getApplicationContext(), mSession.getSessionToken());
 
         mSession.setCallback(new MediaSession.Callback() {
 
+            @Override
+            public void onPause() {
+                super.onPause();
+                Thread cThread = new Thread(new ClientThread());
+                cThread.start();
+            }
 
-                                 @Override
-                                 public void onPause() {
-                                     super.onPause();
-                                     Log.e("MediaPlayerService", "onPause");
-                                     //buildNotification(generateAction(android.R.drawable.ic_media_play, "Pause", ACTION_PAUSE));
+            @Override
+            public void onPlay() {
+                super.onPlay();
+                buildNotification(generateAction(android.R.drawable.ic_media_play, "Play", ACTION_PAUSE));
+            }
 
+            @Override
+            public void onStop() {
 
-                                         Thread cThread = new Thread(new ClientThread());
-                                         cThread.start();
-
-
-
-
-                                 }
-                                 @Override
-                                 public void onPlay() {
-                                     super.onPlay();
-                                     Log.e("MediaPlayerService", "onPlay");
-
-                                     buildNotification(generateAction(android.R.drawable.ic_media_play, "Play", ACTION_PAUSE));
-
-
-
-
-
-
-                                    /* new Thread(new Runnable() {
-                                         @Override
-                                         public void run() {
-
-                                             //Your logic that service will perform will be placed here
-                                             //In this example we are just looping and waits for 1000 milliseconds in each loop.
-                                             while(true){
-                                                 try {
-                                                     Thread.sleep(1000);
-                                                 } catch (Exception e) {
-                                                 }
-                                                 Log.e("MediaPlayerService", "Executando");
-                                             }
-                                         }
-                                     }).start();*/
-
-
-                                 }
-
-                                 @Override
-                                 public void onStop() {
-                                     super.onStop();
-                                     Log.e("MediaPlayerService", "onStop");
-                                     //Stop media player here
-                                     NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-                                     notificationManager.cancel(1);
-                                     Intent intent = new Intent(getApplicationContext(), MediaPlayerService.class);
-                                     stopService(intent);
-                                 }
-                             }
-        );
+            }
+        });
     }
-
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public boolean onUnbind(Intent intent) {
